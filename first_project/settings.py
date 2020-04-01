b35095d05efb553570639877c487db1802f5317a
Lumizz <yiansh15@gmail.com>
2020-04-01 21:18:38 +1100

    titanic

--- first_project/settings.py
+++ first_project/settings.py
@@ -118,4 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 import os.path
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join('static'),) 
+STATICFILES_DIRS = (os.path.join('static'),)
